e1c6ae44aa1baf69e77acfbb75bb9c4b794c2e3d
Giorgi Tsvimitidze <25959064+lostinbinary@users.noreply.github.com>
2023-06-14 23:14:53 +0400

    add currency to manually

--- scripts/app.module.js
+++ scripts/app.module.js
@@ -9,19 +9,15 @@ $.getJSON("https://www.floatrates.com/daily/try.json", function(currencies) {
 });
 
 // get input from popup
-/*chrome.runtime.onMessage.addListener(
+chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if(!sender.tab) {
         tl = request.greeting;
         // console.log(request.greeting)
         $('.new-price').remove();
         currency();
-        
-        // console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension: " + request.greeting);
-        // if (request.greeting == "hello1")
-        //     sendResponse({farewell: "goodbye"});
     }
-});*/
+});
 
 // loop
 setInterval(function(){ currency(); }, 1000);
@@ -29,7 +25,8 @@ setInterval(function(){ currency(); }, 1000);
 // convert TL to LARI
 function currency() {
 
-    let priceBox = $(document).find('.prc-box-dscntd > font > font');
+    // main page
+    let priceBox = $(document).find('.prc-box-dscntd > font > font, .price-box > font > font');
     $.each(priceBox, function(key, element) {
         let price = $(element).text().replaceAll(',','');
         let isPrice = $(element).find('.new-price');
@@ -37,7 +34,8 @@ function currency() {
             $(element).append('<font class="new-price">(' + (parseInt(price) * tl).toFixed(0) + ' GEL)</font>');
         }
     });
-    
+
+    // detail page
     let detailPriceBox = $(document).find('.prc-dsc > font > font');
     let price = detailPriceBox.text().replaceAll(' TL','');
     let isPrice = $(detailPriceBox).find('.new-price');
--- scripts/index.module.js
+++ scripts/index.module.js
@@ -2,22 +2,37 @@
 let currency = $('[name="currency"]').val();
 
 if(currency.length == 0) {
-    $.getJSON("currency.txt", function(currency) {
-        $('[name="currency"]').val(currency);
+    // $.getJSON("currency.txt", function(currency) {
+    //     $('[name="currency"]').val(currency);
+    //     sendCurrencyToApp();
+    // });
+    chrome.storage.local.get(["lari_in_lira"]).then((result) => {
+        $('[name="currency"]').val(result.lari_in_lira);
         sendCurrencyToApp();
     });
 }
 
 $('#btn').on('click', function() {
     sendCurrencyToApp();
+    let lari_in_lira = $('[name="currency"]').val()
+    chrome.storage.local.set({ lari_in_lira: lari_in_lira }).then(() => {
+        console.log("Value is set");
+    });
 });
 
+function chromeTabs(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {
+        greeting: $('[name="currency"]').val()
+    }, function(response) {
+        // alert(response.farewell);
+    });
+}
+
 function sendCurrencyToApp() {
+    chrome.tabs.query({active: false, currentWindow: true}, function(tabs) {
+        chromeTabs(tabs);
+    });
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {
-            greeting: $('[name="currency"]').val()
-        }, function(response) {
-            // alert(response.farewell);
-        });
+        chromeTabs(tabs);
     });
 }
