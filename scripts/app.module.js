fd870bef892a849e2adc54ad70a8822e0e15403e
George <25959064+mrbinarie@users.noreply.github.com>
2023-10-20 18:20:42 +0400

    fixed 0 GEL for big prices, add code in OOP

--- scripts/app.module.js
+++ scripts/app.module.js
@@ -1,48 +1,64 @@
 // https://bankofgeorgia.ge/en/currencies
 
-let tl = 0;
+class App {
+    
+    tl = 0;
 
-// get default data
-chrome.storage.local.get(["lari_in_lira"]).then((result) => {
-    tl = result.lari_in_lira;
-});
+    constructor() {
 
-// get input from popup
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if(!sender.tab) {
-            tl = request.greeting;
-            $('.new-price').remove();
-            addCurrency();
-        }
+        let _this =  this;
+        // get default data
+        chrome.storage.local.get(["lari_in_lira"]).then((result) => {
+            _this.tl = result.lari_in_lira;
+        });
+        
+        // get input from popup
+        chrome.runtime.onMessage.addListener(
+            function(request, sender, sendResponse) {
+                if(!sender.tab) {
+                    _this.tl = request.greeting;
+                    $('.new-price').remove();
+                    _this.addCurrency();
+                }
+            }
+        );
+        
+        // loop
+        setInterval(function(){ _this.addCurrency(); }, 1000);
     }
-);
+    addCurrency()
+    {
+        let _this =  this;
+        // main page
+        let priceBox = $(document).find('.prc-box-dscntd font font, .price-box font font');
+        $.each(priceBox, function(key, element) {
+            let price = _this.priceConverter( $(element).text() );
+            let isPrice = $(element).closest('div').find('.new-price');
+            if(isPrice.length == 0) {
+                $(element).closest('div').append('<font class="new-price"> (' + price + ' GEL)</font>');
+            }
+        });
+    
+        // detail page
+        let detailPriceBox = $(document).find('.prc-dsc font font, .pb-summary-total-price font font, .pb-basket-item-price > font font');
+        $.each(detailPriceBox, function(key, element) {
+            let detailPrice = _this.priceConverter( $(element).text() );
+            let isDetailPrice = $(element).closest('div').find('.new-price');
+            if(isDetailPrice.length == 0) {
+                $(element).append('<font class="new-price"> (' + detailPrice + ' GEL)</font>');
+            }
+        });
+            // console.log('done')
+    }
 
-// loop
-setInterval(function(){ addCurrency(); }, 1000);
-
-// convert TL to LARI
-function addCurrency() {
-console.log('asd')
-    // main page
-    let priceBox = $(document).find('.prc-box-dscntd font font, .price-box font font');
-    $.each(priceBox, function(key, element) {
-        let price = $(element).text().replaceAll(',','');
-        let isPrice = $(element).closest('div').find('.new-price');
-        if(isPrice.length == 0 && $.isNumeric(price)) {
-            $(element).closest('div').append('<font class="new-price"> (' + (parseInt(price) * tl).toFixed(0) + ' GEL)</font>');
-        }
-    });
-
-    // detail page
-    let detailPriceBox = $(document).find('.prc-dsc font font, .pb-summary-total-price font font, .pb-basket-item-price > font font');
-    $.each(detailPriceBox, function(key, element) {
-        let detailPrice = $(element).text().replaceAll(' TL','');
-        let isDetailPrice = $(element).closest('div').find('.new-price');
-        if(isDetailPrice.length == 0 && $.isNumeric(detailPrice)) {
-            $(element).append('<font class="new-price"> (' + (parseInt(detailPrice) * tl).toFixed(0) + ' GEL)</font>');
-        }
-    });
-
-    // console.log('done')
+    priceConverter(price) {
+        let _this =  this;
+        const pattern = /[\.,](?=\d{3})/g;
+        let toLari = price.replace(pattern, '');
+        
+        toLari = parseFloat(toLari) * _this.tl;
+        return toLari.toFixed(0);
+    }
 }
+
+let app = new App();
